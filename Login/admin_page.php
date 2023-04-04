27995d6c68d35c3bc6712331430825dc05debce8
jainth185 <117762034+jainth185@users.noreply.github.com>
2023-04-04 16:20:31 +0530

    Add files via upload

---
+++ Login/admin_page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    
+
+    <div class="container">
+        <div class="content">
+            <h3>Hi, <span> Admin </span></h3>
+            <h1>Welcome <span></span></h1>
+            <p>Admin Page</p>
+            <a href="login_form.php" class="btn">Login</a>
+            <a href="login_form.php" class="btn">Login</a> 
+        </div>
+    </div>
+
+</body>
+</html>
